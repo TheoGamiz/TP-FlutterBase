965dfcea6aad9458ea976292955a170c62eafebc
Theo Gmz <theogamiz31@gmail.com>
2023-06-03 22:13:36 +0200

    ajout du post item

--- lib/home_screen/home_screen.dart
+++ lib/home_screen/home_screen.dart
@@ -1,17 +1,77 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:tp_flutterbase/home_screen/post_bloc/post_bloc.dart';
+import 'package:tp_flutterbase/home_screen/repository/post_repository.dart';
+
+import 'models/post.dart';
+import 'post_item.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen
-({super.key});
+  const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Test"),
-      ),
+    return BlocProvider(
+      create: (context) => PostsBloc(repository:  RepositoryProvider.of<PostsRepository>(context),
+      )..add(GetAllPosts(10)),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Test"),
+        ),
+        body: BlocBuilder<PostsBloc, PostsState>(
+              builder: (context, state) {
+                switch (state.status) {
+                  case PostsStatus.initial:
+                    return const SizedBox();
+                  case PostsStatus.loading:
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  case PostsStatus.error:
+                    return Center(
+                      child: Text(state.error),
+                    );
+                  case PostsStatus.success:
+                    final posts = state.posts;
+
+                    if (posts.isEmpty) {
+                      return const Center(
+                        child: Text('Aucun produit'),
+                      );
+                    }
+
+                    return ListView.builder(
+                      itemCount: posts.length,
+                      itemBuilder: (context, index) {
+                        final post = posts[index];
+                        return PostItem(
+                          post: post,
+                          onTap: () => _onPostTap(context, post),
+                        );
+                      },
+                    );
+                }
+              },
+            ),
+            floatingActionButton: FloatingActionButton(
+              child: const Icon(Icons.refresh),
+              onPressed: () => _onRefreshList(context),
+            ),
+          )
     );
   }
+
+
+
+  void _onRefreshList(BuildContext context) {
+    final postsBloc = BlocProvider.of<PostsBloc>(context);
+    postsBloc.add(GetAllPosts(10));
+  }
+
+  void _onPostTap(BuildContext context, Post post) {
+    //PostDetailScreen.navigateTo(context, post);
+  }
 }
+      
