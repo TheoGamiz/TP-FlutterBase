45ad5f3da9ad10fb88ccc15f77ca5da191c7bd7b
Theo Gmz <theogamiz31@gmail.com>
2023-06-04 14:30:01 +0200

    ajout des tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,25 +6,71 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:tp_flutterbase/home_screen/data_sources/posts_data_source.dart';
+import 'package:tp_flutterbase/home_screen/models/post.dart';
+import 'package:tp_flutterbase/home_screen/repository/post_repository.dart';
 
 import 'package:tp_flutterbase/main.dart';
 
+
+
+
+class EmptyRemoteDataSource extends PostsDataSource {
+  @override
+  Future<List<Post>> getPosts() async {
+    await Future.delayed(const Duration(seconds: 2));
+    return [];
+  }
+}
+
+class FailingRemoteDataSource extends PostsDataSource {
+  @override
+  Future<List<Post>> getPosts() async {
+    await Future.delayed(const Duration(seconds: 2));
+    throw Exception('Error');
+  }
+}
+
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  testWidgets('Posts Screen with succee', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      RepositoryProvider(
+        create: (context) => PostsRepository(
+          remoteDataSource: EmptyRemoteDataSource(),
+        ),
+        
+      ),
+    );
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
     await tester.pump();
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.text('Posts'), findsOneWidget);
+    await tester.pump();
+    expect(find.byType(CircularProgressIndicator), findsOneWidget);
+
+    await tester.pump(const Duration(seconds: 3));
+    expect(find.text('Aucun post'), findsOneWidget);
+  });
+
+  testWidgets('Posts Screen with error', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      RepositoryProvider(
+        create: (context) => PostsRepository(
+          remoteDataSource: FailingRemoteDataSource(),
+        ),
+        
+      ),
+    );
+
+    await tester.pump();
+
+    expect(find.text('Posts'), findsOneWidget);
+    await tester.pump();
+    expect(find.byType(CircularProgressIndicator), findsOneWidget);
+
+    await tester.pump(const Duration(seconds: 3));
+    expect(find.text(Exception('Error').toString()), findsOneWidget);
   });
 }
